474f1b77bd03a123fbe290165441101c61aa102b
unknown <ravikr.sharma2007@gmail.com>
2025-11-29 08:00:52 +0530

    phone number and mail added

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent } from "./ui/card";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { GithubIcon, LinkedinIcon, Loader } from "lucide-react";
+import { GithubIcon, LinkedinIcon, Loader, MailIcon, PhoneCallIcon } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 
 const ContactSection = () => {
@@ -69,14 +69,31 @@ const ContactSection = () => {
           Get in Touch
         </h2>
 
-        <div className="bg max-w-xl mx-auto py-2">
-          <a target="_blank" href="https://www.linkedin.com/in/ravi-kr-sharma-1122a3299/" className="pl-2 pr-3 sm:px-3 inline-block">
-            <LinkedinIcon strokeWidth={1.3} className="w-8 h-8 hover:text-emerald-500" />
+
+        <div className="flex gap-2 items-start md:items-center flex-col py-10 sm:py-6 font-serif text-lg">
+          <a href="tel:+918920878094" className="flex-center gap-2 hover:text-emerald-600">
+            <PhoneCallIcon />+91 8920878094
           </a>
-          <a target="_blank" href="https://github.com/Ravikumarsharma07/" className="pl-2 pr-4 sm:px-4 inline-block">
-            <GithubIcon strokeWidth={1.3} className="w-8 h-8 hover:text-emerald-500" />
+          <a href="mailto:ravikrsharma207@gmail.com" className="flex-center gap-2 hover:text-emerald-600">
+            <MailIcon />ravikrsharma207@gmail.com
           </a>
         </div>
+        
+        {/* social profile links, github and linkedin  */}
+        <div className="bg max-w-xl mx-auto py-2 flex gap-2 sm:gap-3 pl-1">
+          <a target="_blank" href="https://www.linkedin.com/in/ravi-kr-sharma-1122a3299/">
+            <LinkedinIcon strokeWidth={1.3} className="w-8 h-8 hover:text-emerald-500" />
+          </a>
+          <a target="_blank" href="https://github.com/Ravikumarsharma07/">
+            <GithubIcon strokeWidth={1.3} className="w-8 h-8 hover:text-emerald-500" />
+          </a>
+          <a target="_blank" href="https://wa.me/918920878094" className="text-emerald-500 fill-white hover:fill-emerald-600">
+            {/* <GithubIcon strokeWidth={1.3} className="w-8 h-8 hover:text-emerald-500" /> */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M16.6 14c-.2-.1-1.5-.7-1.7-.8c-.2-.1-.4-.1-.6.1c-.2.2-.6.8-.8 1c-.1.2-.3.2-.5.1c-.7-.3-1.4-.7-2-1.2c-.5-.5-1-1.1-1.4-1.7c-.1-.2 0-.4.1-.5c.1-.1.2-.3.4-.4c.1-.1.2-.3.2-.4c.1-.1.1-.3 0-.4c-.1-.1-.6-1.3-.8-1.8c-.1-.7-.3-.7-.5-.7h-.5c-.2 0-.5.2-.6.3c-.6.6-.9 1.3-.9 2.1c.1.9.4 1.8 1 2.6c1.1 1.6 2.5 2.9 4.2 3.7c.5.2.9.4 1.4.5c.5.2 1 .2 1.6.1c.7-.1 1.3-.6 1.7-1.2c.2-.4.2-.8.1-1.2l-.4-.2m2.5-9.1C15.2 1 8.9 1 5 4.9c-3.2 3.2-3.8 8.1-1.6 12L2 22l5.3-1.4c1.5.8 3.1 1.2 4.7 1.2c5.5 0 9.9-4.4 9.9-9.9c.1-2.6-1-5.1-2.8-7m-2.7 14c-1.3.8-2.8 1.3-4.4 1.3c-1.5 0-2.9-.4-4.2-1.1l-.3-.2l-3.1.8l.8-3l-.2-.3c-2.4-4-1.2-9 2.7-11.5S16.6 3.7 19 7.5c2.4 3.9 1.3 9-2.6 11.4"/></svg>
+          </a>
+        </div>
+
+        {/* form to send mail  */}
         <Card className="max-w-xl mx-auto bg-transparent border-[#919191]">
           <CardContent className="p-6">
             <form onSubmit={handleSubmit} className="space-y-6">
